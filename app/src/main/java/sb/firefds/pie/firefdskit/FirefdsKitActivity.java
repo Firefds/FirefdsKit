35d63e35441d8298a315d4b86fd7a918c9774ec4
Shauli Bracha <shauli.bracha@hotmail.com>
2019-03-25 16:45:07 +0200

    Fix system preference listener

--- app/src/main/java/sb/firefds/pie/firefdskit/FirefdsKitActivity.java
+++ app/src/main/java/sb/firefds/pie/firefdskit/FirefdsKitActivity.java
@@ -544,15 +544,18 @@ public class FirefdsKitActivity extends AppCompatActivity
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
             super.onSharedPreferenceChanged(sharedPreferences, key);
-            AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
-            TextView tv = new TextView(getContext());
-            tv.setMovementMethod(LinkMovementMethod.getInstance());
-            tv.setText(R.string.navigation_bar_color_dialog_message);
-            tv.setPadding(16, 16, 16, 16);
-            builder.setTitle(R.string.navigation_bar_color_dialog_title)
-                    .setView(tv)
-                    .setNeutralButton("OK", (dialog, id) -> dialog.dismiss())
-                    .show();
+
+            if (key.equals(PREF_NAVIGATION_BAR_COLOR)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
+                TextView tv = new TextView(getContext());
+                tv.setMovementMethod(LinkMovementMethod.getInstance());
+                tv.setText(R.string.navigation_bar_color_dialog_message);
+                tv.setPadding(16, 16, 16, 16);
+                builder.setTitle(R.string.navigation_bar_color_dialog_title)
+                        .setView(tv)
+                        .setNeutralButton("OK", (dialog, id) -> dialog.dismiss())
+                        .show();
+            }
         }
     }
 
