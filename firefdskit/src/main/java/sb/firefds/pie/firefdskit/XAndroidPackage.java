db3d56172d9908bef302cf6ad0a52045b633d5cd
Shauli Bracha <shauli.bracha@hotmail.com>
2020-02-15 22:13:53 -0500

    Added option to change default behavior of reboot to reboot to recovery

--- firefdskit/src/main/java/sb/firefds/pie/firefdskit/XAndroidPackage.java
+++ firefdskit/src/main/java/sb/firefds/pie/firefdskit/XAndroidPackage.java
@@ -30,6 +30,7 @@ import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 
+import static sb.firefds.pie.firefdskit.utils.Preferences.PREF_DEFAULT_REBOOT_BEHAVIOR;
 import static sb.firefds.pie.firefdskit.utils.Preferences.PREF_DISABLE_SECURE_FLAG;
 import static sb.firefds.pie.firefdskit.utils.Preferences.PREF_DISABLE_SIGNATURE_CHECK;
 import static sb.firefds.pie.firefdskit.utils.Preferences.PREF_HIDE_USB_NOTIFICATION;
@@ -46,6 +47,7 @@ public class XAndroidPackage {
     private static final String INSTALLER = "com.android.server.pm.Installer";
     private static final String STATUS_BAR_MANAGER_SERVICE = "com.android.server.statusbar.StatusBarManagerService";
     private static final String USB_HANDLER = "com.android.server.usb.UsbDeviceManager.UsbHandler";
+    private static final String SHUTDOWN_THREAD = "com.android.server.power.ShutdownThread";
     @SuppressLint("StaticFieldLeak")
     private static Context mPackageManagerServiceContext;
     private static boolean isFB;
@@ -53,6 +55,24 @@ public class XAndroidPackage {
     public static void doHook(final XSharedPreferences prefs, final ClassLoader classLoader) {
 
         try {
+            if (prefs.getBoolean(PREF_DEFAULT_REBOOT_BEHAVIOR, false)) {
+                Class<?> shutdownThreadClass = XposedHelpers.findClass(SHUTDOWN_THREAD, classLoader);
+                XposedHelpers.findAndHookMethod(shutdownThreadClass,
+                        "shutdownInner",
+                        Context.class,
+                        boolean.class,
+                        boolean.class,
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) {
+                                String mRebootReason = (String) XposedHelpers.getStaticObjectField(shutdownThreadClass, "mRebootReason");
+                                boolean mReboot = XposedHelpers.getStaticBooleanField(shutdownThreadClass, "mReboot");
+                                if (mReboot && mRebootReason.equals("userrequested")) {
+                                    XposedHelpers.setStaticObjectField(shutdownThreadClass, "mRebootReason", "recovery");
+                                }
+                            }
+                        });
+            }
             if (prefs.getBoolean(PREF_DISABLE_SECURE_FLAG, false)) {
                 Class<?> windowStateClass = XposedHelpers.findClass(WINDOW_STATE, classLoader);
 
