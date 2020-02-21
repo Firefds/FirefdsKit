e7eaf31d9d79c9ce5fae7d6c00842b000b40d464
Shauli Bracha <shauli.bracha@hotmail.com>
2020-02-21 10:28:32 -0500

    Removed unused import

--- firefdskit/src/main/java/sb/firefds/pie/firefdskit/dialogs/SaveDialog.java
+++ firefdskit/src/main/java/sb/firefds/pie/firefdskit/dialogs/SaveDialog.java
@@ -10,7 +10,6 @@ import android.widget.EditText;
 import androidx.appcompat.app.AlertDialog;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
