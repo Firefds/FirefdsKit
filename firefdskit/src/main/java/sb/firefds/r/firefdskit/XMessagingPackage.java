3873b58b5511ec3969a4d9e0603db3d21fb6c506
Shauli Bracha <shauli.bracha@hotmail.com>
2020-12-31 13:31:16 -0500

    Fix Disable SMS to MMS conversion

--- firefdskit/src/main/java/sb/firefds/r/firefdskit/XMessagingPackage.java
+++ firefdskit/src/main/java/sb/firefds/r/firefdskit/XMessagingPackage.java
@@ -64,6 +64,7 @@ public class XMessagingPackage {
                 XposedHelpers.findAndHookMethod(messagingFeatureClass,
                         "getSmsToMmsByThreshold",
                         int.class,
+                        int.class,
                         XC_MethodReplacement.returnConstant(!disableSmsToMms));
             } catch (Throwable e1) {
                 XposedBridge.log(e1);
